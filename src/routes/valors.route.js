1a85825bac0aaca4580d3164dcc13c3882134c65
felipeHELPNET <felipehelpnet@gmail.com>
2023-08-28 19:45:49 -0300

    Auth_e_JTW

--- src/routes/valors.route.js
+++ src/routes/valors.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { create, findAll } from "../controllers/valors.controller.js";
 
-router.post("/", create)
-router.get("/", findAll)
+router.post("/", authMiddleware, create);
+router.get("/", findAll);
 
 export default router;
