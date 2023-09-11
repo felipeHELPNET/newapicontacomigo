f828923ab147ee5c1a42813b81ba22c5474c6bc3
felipeHELPNET <felipehelpnet@gmail.com>
2023-09-11 00:47:21 -0300

    init swagger

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import registersRoute from "./src/routes/registers.route.js";
-
-
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
@@ -17,5 +16,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/registers", registersRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
