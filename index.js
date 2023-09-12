e6ec1a07f3bf9398e5f65477fc7c609c9a77765c
felipeHELPNET <felipehelpnet@gmail.com>
2023-09-12 16:33:02 -0300

    remove swagger

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import registersRoute from "./src/routes/registers.route.js";
-import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
@@ -16,6 +15,5 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/registers", registersRoute);
-app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
