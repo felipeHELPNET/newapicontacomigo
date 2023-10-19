6c4d4b566b8fbf465c998b554e796168a554a6d9
felipeHELPNET <felipehelpnet@gmail.com>
2023-10-18 21:08:08 -0300

    up server.js reinstalação do CORS

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-import "dotenv/config";
-import app from "./app.js";
+import express from "express";
+import connectDatabase from "./src/database/database.js";
+import dotenv from "dotenv";
+import userRoute from "./src/routes/user.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import registerRouter from "./src/routes/register.route.js";
+import cors from "cors";
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+connectDatabase();
+app.use(
+    cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+app.use(express.json());
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
+app.use("/registers", registerRouter);
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+
+
+
+
+
+
+// código do server.js:
+
+//import "dotenv/config";
+//import app from "./app.js";
+//
+//const port = process.env.PORT || 3001;
+//app.listen(port, () => console.log(`Server running on port: ${port}`));
