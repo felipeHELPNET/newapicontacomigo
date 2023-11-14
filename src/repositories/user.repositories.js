bf5885e29e12999c1a9435cc43de349c6d1c07bc
felipeHELPNET <felipehelpnet@gmail.com>
2023-11-14 15:22:34 -0300

    up avatar background

--- src/repositories/user.repositories.js
+++ src/repositories/user.repositories.js
@@ -2,19 +2,21 @@ import User from "../models/User.js";
 
 const findByEmailUserRepository = (email) => User.findOne({ email: email });
 
-const createUserRepository = ({ name, username, email, password }) =>
+const createUserRepository = ({ name, username, email, password, avatar, background }) =>
   User.create({
     name,
     username,
     email,
     password,
+    avatar,
+    background,
   });
 
 const findAllUserRepository = () => User.find();
 
 const findByIdUserRepository = (idUser) => User.findById(idUser);
 
-const updateUserRepository = (id, name, username, email, password) =>
+const updateUserRepository = (id, name, username, email, password, avatar, background) =>
   User.findOneAndUpdate(
     {
       _id: id,
@@ -24,6 +26,8 @@ const updateUserRepository = (id, name, username, email, password) =>
       username,
       email,
       password,
+      avatar,
+      background
     },
     {
       rawResult: true,
