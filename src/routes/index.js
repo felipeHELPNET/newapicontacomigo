374e1f43c2a988c2d2c2dc7a7a0e8a9ea22bb78a
felipeHELPNET <felipehelpnet@gmail.com>
2023-12-22 11:16:28 -0300

    resolveCache

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,17 @@ import swaggerRouter from "./swagger.route.js";
 
 const router = Router();
 
+// Middleware global permitindo caching por 1 hora
 router.use((req, res, next) => {
-    res.header('Cache-Control', 'public, max-age=3600');
-    next();
-  });
+  // Remova explicitamente os cabeçalhos no-cache e Pragma
+  res.removeHeader('Cache-Control');
+  res.removeHeader('Pragma');
+  
+  // Adicione o cabeçalho Cache-Control desejado
+  res.header('Cache-Control', 'public, max-age=3600');
+
+  next();
+});
 
 router.use("/user", userRouter);
 router.use("/registers", registerRouter);
@@ -17,3 +24,25 @@ router.use("/auth", authRouter);
 router.use("/doc", swaggerRouter);
 
 export default router;
+
+/* import { Router } from "express";
+import userRouter from "./user.route.js";
+import registerRouter from "./register.route.js";
+import authRouter from "./auth.route.js";
+import swaggerRouter from "./swagger.route.js";
+
+const router = Router();
+
+router.use((req, res, next) => {
+  res.header('Cache-Control', 'public, max-age=3600');
+  next();
+});
+
+router.use("/user", userRouter);
+router.use("/registers", registerRouter);
+router.use("/auth", authRouter);
+router.use("/doc", swaggerRouter);
+
+export default router; */
+
+
